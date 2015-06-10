80f04aa43f30bdd737f9fc0bc9eb213590f5dc13
Samo Pogacnik <samo_pogacnik@t-2.net>
2015-06-10 17:25:14 +0200

    Added initial support to import components (development part) into the individual build process.

--- build.sh
+++ build.sh
@@ -11,38 +11,51 @@
 #
 
 set -e
-echo "called: "$0
+export pro="> "
+export tab="${tab}-"
+pre="${tab}${pro}"
+echo "${pre}CALLED: "$0
 build_u2up_DIR=`dirname $0`
-echo "from build dir: "$PWD
+echo "${pre}from build dir: "$PWD
 comp_build_DIR=$PWD
 
 if [ "x"$1 != "x" ]
 then
-	comp_source_DIR=$1
-	echo "source dir: "$comp_source_DIR
+	cd $1
+	comp_source_DIR=$PWD
+	cd - > /dev/null
+	echo "${pre}source dir: "$comp_source_DIR
 else
-	echo "ERROR: The first parameter must be source dir!"
+	echo "${pre}ERROR: The first parameter must be source dir!"
 	exit 1
 fi
 
 comp_specs_DIR=$comp_source_DIR/comp_specs
-echo "comp_specs dir: "$comp_specs_DIR
+echo "${pre}comp_specs dir: "$comp_specs_DIR
 comp_install_DIR=$comp_build_DIR/install
-echo "install dir: "$comp_install_DIR
+echo "${pre}install dir: "$comp_install_DIR
 
 if [ "x"$2 != "x" ]
 then
-	comp_repos_DIR=$2
+	cd $2
+	comp_repos_DIR=$PWD
+	cd - > /dev/null
 	mkdir -p $comp_repos_DIR
-	echo "repository dir: "$comp_repos_DIR
+	echo "${pre}repository dir: "$comp_repos_DIR
 else
 	comp_repos_DIR=
 fi
 
+. $comp_specs_DIR/name
+
+echo "${pre}Component name: "$comp_name
+
 . $comp_specs_DIR/version
 
 comp_version=$comp_version_MAJOR"."$comp_version_MINOR"."$comp_version_PATCH
-echo "comp_version: "$comp_version
+echo "${pre}Component version: "$comp_version
+
+$build_u2up_DIR/import_required.sh $comp_specs_DIR $comp_build_DIR $comp_repos_DIR
 
 . $comp_specs_DIR/build
 
@@ -50,13 +63,13 @@ echo "comp_version: "$comp_version
 
 for package_name in "${COMP_PACKAGES[@]}"
 do
-	echo "package_name: "$package_name
+	echo "${pre}Component's package_name: "$package_name
 	if [ "x"$package_name != "x" ]
 	then
 		subst="COMP_PACKAGE_${package_name}[@]"
 		for package_type in "${!subst}"
 		do
-			echo "package_type: "$package_type
+			echo "${pre}package_type: "$package_type
 			if [ "x"$package_type != "x" ]
 			then
 				case $package_type in
@@ -71,7 +84,7 @@ do
 					;;
 				esac
 				subsubst="COMP_PACKAGE_${package_name}_${package_type}[@]"
-				echo "package \""$comp_package_name"\" content: "${!subsubst}
+				echo "${pre}package \""$comp_package_name"\" content: "${!subsubst}
 #set -x
 				cd $comp_install_DIR
 				tar czvf $comp_build_DIR/files.tgz ${!subsubst}
@@ -80,13 +93,13 @@ do
 				$build_u2up_DIR/create_package.sh $comp_package_name-$comp_version files.tgz
 				if [ "x"$comp_repos_DIR != "x" ]
 				then
-					echo "Copy package to the common repository: "$comp_repos_DIR
+					echo "${pre}Copy package to the common repository: "$comp_repos_DIR
 					cp -pf $comp_package_name-$comp_version* $comp_repos_DIR/
 				else
-					echo "The second parameter (a common repository path) not provided (package not copied)!"
+					echo "${pre}The second parameter (a common repository path) not provided (package not copied)!"
 				fi
 				cd - > /dev/null
-				echo "Package DONE!"
+				echo "${pre}Package DONE!"
 			fi
 		done
 	fi
--- create_package.sh
+++ create_package.sh
@@ -11,21 +11,39 @@
 #
 
 set -e
+export tab="${tab}-"
+pre="${tab}${pro}"
+echo "${pre}CALLED: "$0
 
 pkg_filename=$1.u2up
 sha_filename=$1.sha
-echo "Creating U2UP installation package: "$pkg_filename
+echo "${pre}Creating U2UP installation package: "$pkg_filename
 cat > $pkg_filename << EOF
 #!/bin/bash
 
-echo "Running U2UP installation package: "\$0
-sed '0,/^__ARCHIVE_TGZ__$/d' \$0 | tar xzv
+if [ "x"\$1 != "x" ]
+then
+	case \$1 in
+	view)
+		tar_options="tzv"
+		;;
+	extract)
+		echo "Extracting U2UP installation package: "\$0
+		tar_options="xzv"
+		;;
+	esac
+else
+	# default is to view content:
+	tar_options="tzv"
+fi
+
+sed '0,/^__ARCHIVE_TGZ__$/d' \$0 | tar \$tar_options
 exit 0
 __ARCHIVE_TGZ__
 EOF
 chmod 755 $pkg_filename
 cat $2 >> $pkg_filename
-echo "Creating U2UP package SHA-512 hash: "$sha_filename
+echo "${pre}Creating U2UP package SHA-512 hash: "$sha_filename
 sha512sum -b $pkg_filename > $sha_filename
 
 exit 0
